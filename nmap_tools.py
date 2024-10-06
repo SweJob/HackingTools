5c9274bf11eafda7856a747173a25b27096aa4be
SweJob <31406955+SweJob@users.noreply.github.com>
2024-10-06 09:04:46 +0200

    added working menu function to misc_tools

--- nmap_tools.py
+++ nmap_tools.py
@@ -1,8 +1,17 @@
 import ipaddress
 import nmap
 import misc_tools
+import sys
+import getopt
 
 # Check if running on windows, otherwise assume linux
 WINDOWS = misc_tools.is_windows()
 
+# get a list of command line arguments
+no_of_arguments = len(sys.argv)
 
+# compare arguments with list of valid arguments
+
+
+╠
+   

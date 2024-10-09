2739ba77db89ec7a0d189c51b60fc6179c92c7e3
SweJob <31406955+SweJob@users.noreply.github.com>
2024-10-09 22:56:04 +0200

    Added output window and moved  status to bootom. Added colors for diffrent parts of screen

--- nmap_tools.py
+++ nmap_tools.py
@@ -11,10 +11,10 @@ Optional:
 """
 import os
 import ipaddress
-import pprint
+# import pprint
 import time
 import nmap
-
+from colorama import Fore, Back, Style
 from swejob_tools import misc_tools
 
 # Global variables
@@ -28,13 +28,32 @@ settings_saved = True
 status_msg = "Ready"
 scan_res = []
 
+def set_status_msg(msg):
+    """
+    Set status_msg to msg or 'Ready' if msg is empty 
+    """
+    global status_msg
+    if msg == "":
+        status_msg = "Ready"
+    else:
+        status_msg = msg
+
 def is_valid_ip_address(ip_string):
-    """ Return true if a valid ip adress is provided as an argument """    
+    """ 
+    Return true if a valid ip adress is provided as an argument 
+    """
     try:
         ipaddress.ip_address(ip_string)
         return True
     except ValueError:
         return False
+
+def settings_changed(changed):
+    """ 
+    set global settings_saved to False
+    """
+    global settings_saved
+    settings_saved = not changed
 
 def set_ip_address():
     """ 
@@ -43,20 +62,21 @@ def set_ip_address():
     """
     # Accessing the global variables
     global ip_addr_str
-    global settings_saved
-    global status_msg
-    status_msg = "Getting an IP address and mask from user"
-    print_status_msg()
     global subnet_mask_str
-
+    set_status_msg("Getting an IP address and mask from user")
+    print_status_msg()
     valid_ip_addr = False
     # loop until valid adress is entered
     while not valid_ip_addr:
-        user_ip_addr = input("\x1b[4;43HEnter IP-address[aaa.bbb.ccc.ddd][/0-32]]:")
+        # Clear previous entry
+        misc_tools.pos_print(25,1," "*80)
+        # Get input , prompt under status message
+        user_ip_addr = input("\x1b[25;1HEnter IP-address[aaa.bbb.ccc.ddd][/0-32]]: ")
         #check if entered adress minus any mask provided is valid
         valid_ip_addr = is_valid_ip_address(user_ip_addr.split("/")[0])
     ip_addr_str = user_ip_addr.split("/")[0]
-    settings_saved = False
+    # A setting was changed
+    settings_changed(True)
 
     #Check if any mask is provided
     if "/" in user_ip_addr:
@@ -65,19 +85,19 @@ def set_ip_address():
             # If that was a number between 0 and 32
             subnet_mask_str = user_ip_addr.split("/")[1]
             if not(subnet_mask_str.isdigit() and int(subnet_mask_str) in range(0,33)):
-                status_msg = "Incorrect subnet. The default 32-bit mask will be used"
+                set_status_msg("Incorrect subnet. The default 32-bit mask will be used")
                 subnet_mask_str = "32"
         else:
-            status_msg= "No subnet value provided. The default 32-bit mask will be used"
+            set_status_msg("No subnet value provided. The default 32-bit mask will be used")
             subnet_mask_str = "32"
     else:
-        status_msg = "No mask divider('/') provided. The default 32-bit mask will be used"
-    print_status_msg()
-    time.sleep(2)
+        set_status_msg("No mask divider('/') provided. The default 32-bit mask will be used")
+    print_status_msg(1)
+    # Clear input line
+    misc_tools.pos_print(25,1," "*80)
 
 def scan_target(ip_address,subnet_mask, scan_flags,output_flags):
     """ Scan target save result in scan_res"""
-    global status_msg
     global scan_res
     # Add the path to nmap.exe if running on windows.
     # THIS PATH MIGHT NEED TO BE ALTERED FOR YOUR SYSTEM
@@ -86,34 +106,31 @@ def scan_target(ip_address,subnet_mask, scan_flags,output_flags):
     else:
         scan_obj = nmap.PortScanner()
 
-    global scan_res
     host_str = ip_address  + "/" + subnet_mask
     # Update status message
-    status_msg = f"Scanning {host_str}"
+    set_status_msg(f"Scanning {host_str}")
     print_status_msg()
 
     scan_obj.scan(host_str, arguments=scan_flag_str+output_flag_str)
     scan_res = scan_obj[ip_address]
 
     # Update status message
-    status_msg = f"Scan of {host_str} is complete"
-    print_status_msg()
+    set_status_msg(f"Scan of {host_str} is complete")
+    print_status_msg(1)
 
 def set_scan_flags():
     """
     User input which flags to select
     """
-    global status_msg
-    status_msg = "Running set_scan_flags"
-    print_status_msg()
+    set_status_msg("Running set_scan_flags")
+    print_status_msg(1)
 
 def set_output_flags():
     """ 
     User input which output flags to select
     """
-    global status_msg
-    status_msg = "Running set_output_flags"
-    print_status_msg()
+    set_status_msg("Running set_output_flags")
+    print_status_msg(1)
 
 def scan_ip_address():
     """ 
@@ -125,65 +142,81 @@ def display_scan_result():
     """
     Print the result of the scan to the screen
     """
-    global status_msg
-    status_msg = "Printing the result from scan"
+    set_status_msg("Printing the result from scan")
     print_status_msg()
+    # Format scan result data to desired output
+    scan_result = [
+        ("The result of the scan of ",ip_addr_str,"/",subnet_mask_str,": ")
+    ]
     
-    output_windows(scan_result)
+    # Send formated data to ouput window
+    output_window(scan_result)
 
 def save_scan_result():
     """ 
     Save scan result to file
     """
-    global status_msg
-    status_msg = "Running save_scan_result"
-    print_status_msg()
+    set_status_msg("Running save_scan_result")
+    print_status_msg(1)
 
 def load_scan_settings():
     """ 
     Load settings from file
     """
-    global status_msg
-    status_msg = "Running load_scan settings"
-    print_status_msg()
+    set_status_msg("Running load_scan settings")
+    print_status_msg(1)
 
 def save_scan_settings():
     """
     Save settings from file
     """
-    global status_msg
-    status_msg = "Running save_scan settings"
-    print_status_msg()
+    set_status_msg("Running save_scan settings")
+    print_status_msg(1)
 
 def display_help():
     """ 
     Print help to the screen
     """
-    global status_msg
-    status_msg = "Running display_help"
-    print_status_msg()
+    set_status_msg("Running display_help")
+    print_status_msg(1)
 
-def print_status_msg():
+def print_status_msg(delay=0):
     """ 
     Print value of status_msg below menu
     """
     # print status_msg
-    misc_tools.print_window([("Status: "+status_msg,)],1,42,75,True)
-    time.sleep(1)
+    print(Fore.GREEN)    
+    misc_tools.print_window([("Status: "+status_msg,)],26,1,121,True)
+    print(Fore.RESET)
+    time.sleep(delay)
 
 def output_window(output_text:list):
-    misc_tools.print_window()
+    """ 
+    Prints provided string in output window under status message
+    """
+    print(Fore.LIGHTYELLOW_EX)
+    misc_tools.print_window(output_text,1,42,80,True)
+    print(Fore.RESET)
+
+def clear_output_window():
+    """ 
+    Clears output window but keeps frame
+    """
+    output_text = []
+    for line in range (21):
+        output_text.append((" ",))
+    output_window(output_text)
     
 def settings_window():
     """ 
-    Display thh setting swindow
+    Display the settings swindow
     """
     # Make a string from the status of settings_saved
     save_string = "Settings are"
     if not settings_saved:
         save_string = save_string + " not"
     save_string = save_string + " saved"
-    
+
     #create list for displaying settings
     setting_stats = [
         ("Current settings",),
@@ -195,8 +228,10 @@ def settings_window():
         (save_string,)
     ]
     # print the settings window in the upper left corner
+    print(Fore.BLUE)
     misc_tools.print_window(setting_stats,1,1,40,True)
-    
+    print(Fore.RESET)
+
 # Constant with list of tuple containing:
 # Selector, menu item and funcion to run
 MAIN_MENU_LIST = [
@@ -216,8 +251,7 @@ def main_menu():
     """ 
     Update status message, print menu and handle response
     """
-    global status_msg
-    status_msg = "Waiting for input. Select from main menu."
+    set_status_msg("Waiting for input. Select from main menu.")
     print_status_msg()
     # print menu and wait for user selection
     misc_tools.menu(MAIN_MENU_LIST,5,32,10,1,menu_header="Nmap Tools by SweJob")
@@ -226,15 +260,14 @@ def main():
     """ 
     Main program
     """
-    global status_msg
-
+    # Clear Screen
+    os.system('cls')
+    clear_output_window()
     while True:
-        # Clear Screen
-        os.system('cls')
         # Print the settings window upper left corner
-        settings_window()        
+        settings_window()
         # Print any recieved status messages at the top, right of setting window
-        print_status_msg()
+        print_status_msg(1)
         # Run main menu function
         main_menu()
 

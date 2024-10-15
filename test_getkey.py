c2e9847c7f41f47ea9b588823b6076846286756b
SweJob <31406955+SweJob@users.noreply.github.com>
2024-10-15 11:11:38 +0200

    Added mode=9 to swejob_tools.get_key(), for raw output from getkey() and edited test_getkey.py to test it

--- test_getkey.py
+++ test_getkey.py
@@ -1,8 +1,6 @@
 from swejob_tools import misc_tools
 
-
-a = misc_tools.get_key()
+a = misc_tools.get_key(mode=9)
 while a != "Q":
-    print(a)
-    a = misc_tools.get_key()
-
+    print(a)    
+    a = misc_tools.get_key(mode=9)
